602d9723d74fd6ad90964d8fe63e454b91a164d5
Muhammad Danish Arfa <mdarfa1803@gmail.com>
2025-05-30 18:38:28 +0800

    add new page

---
+++ src/app/dashboard/student/munaqasyah/schedule/page.tsx
@@ -0,0 +1,7 @@
+export default function StudentTashihSchedulePage() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold mb-4">Jadwal Munaqasyah</h1>
+    </div>
+  );
+}
