70d01e2e548ea5facbe593c17accc8d438d6f2f6
Muhammad Danish Arfa <mdarfa1803@gmail.com>
2025-10-27 18:25:58 +0800

    fix label 2

--- src/app/dashboard/teacher/tashih/request/page.tsx
+++ src/app/dashboard/teacher/tashih/request/page.tsx
@@ -3,7 +3,7 @@ import { TashihRequestForm } from '@/components/teacher/tashih/request/TashihReq
 export default function TeacherTashihFormPage() {
   return (
     <div className="p-4">
-      <h1 className="text-2xl font-bold mb-4">Permintaan Tashih Siswa</h1>
+      <h1 className="text-2xl font-bold mb-4">Pendaftaran Tashih Siswa</h1>
       <TashihRequestForm />
     </div>
   );
